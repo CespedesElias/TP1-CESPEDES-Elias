575286fd054b31ed39eb1f12ab8e45ae5703fdb3
Elias Cespedes <elias.cespedes@rcpo.com.ar>
2024-04-16 23:25:33 -0300

    proyecto terminado

--- index.js
+++ index.js
@@ -4,26 +4,38 @@ import apareo from './src/apareo.js'
 
 // leo los 4 archivos a memoria
 let setA = files.leerArchivoComoString('./in/10NumerosOrdenadosEntre1y50(setA).in');
+console.log("Leyendo archivo (setA):", setA)
 let setB = files.leerArchivoComoString('./in/10NumerosOrdenadosEntre1y50(setB).in');
+console.log("Leyendo archivo (setb):", setB)
 let impares = files.leerArchivoComoString('./in/imparesOrdenadosEntre1y999.in');
+console.log("Leyendo archivo (impares):", impares)
 let pares = files.leerArchivoComoString('./in/paresOrdenadosEntre2y1000.in');
+console.log("Leyendo archivo (pares):", impares)
+console.log("------------------------------------------------------------------")
 
 // preparo los 4 arrays a partir de los archivo leidos
 const arraySetA = arrays.transformarStringEnArrayDeNumeros(setA, ',')
+console.log("mostrando array (setA):", arraySetA)
 const arraySetB = arrays.transformarStringEnArrayDeNumeros(setB, ',')
+console.log("mostrando array (setB):", arraySetB)
 const arrayimpares = arrays.transformarStringEnArrayDeNumeros(impares, ',')
+console.log("mostrando array (impares):", arrayimpares)
 const arrayPares = arrays.transformarStringEnArrayDeNumeros(pares, ',')
+console.log("mostrando array (pares):", arraySetB)
+console.log("------------------------------------------------------------------")
 
 // combino los primeros dos arrays
 const arrayCombinado = apareo.combinarDosArrays(arraySetA, arraySetB)
-console.log(arrayCombinado)
+console.log("mostrando 2 arrays combinados (setA + setB):", arrayCombinado)
+console.log("------------------------------------------------------------------")
 
 // combino los cuatro arrays
 const arrayDeArrays = apareo.combinarNArrays([arraySetA, arraySetB, arrayimpares, arrayPares])
-console.log(arrayDeArrays)
+console.log("mostrando arrays combinados (setA + setB + impares + pares):", arrayDeArrays)
+console.log("------------------------------------------------------------------")
 
-
-
+files.escribirTextoEnArchivo('./in/datosCombinados.txt', arrays.transformarArrayDeNumerosAUnSoloString(arrayDeArrays, ','), false);
+//console.log(files.leerArchivoComoString('./in/datos.txt'));
 //test de funciones.
 /*
  
